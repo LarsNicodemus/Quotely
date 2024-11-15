671976bb37bfb248ea8cdb69cb63a9cc9cbef607
LarsNicodemus <lars@no-19.net>
2024-11-15 19:11:00 +0100

    Initial Commit

---
+++ Quotely/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quotely
+//
+//  Created by Lars Nicodemus on 15.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quotely/QuotelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotelyApp.swift
+//  Quotely
+//
+//  Created by Lars Nicodemus on 15.11.24.
+//
+
+import SwiftUI
+
+@main
+struct QuotelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
